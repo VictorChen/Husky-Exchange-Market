783bc2e735cda78c7d5df573182d9a8151e343c2
Victor Chen <itsVicc@gmail.com>
2012-10-19 11:39:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuskyExchange::Application.config.secret_token = '9066d35dcee71b95a4d8eb8e29f3ee603ce5c8b972c72f42ce54dca6e9501b2fd68cee30707abe6919b816ce47a002e6195f43399324173845f73345cd843030'
