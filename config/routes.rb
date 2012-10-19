3dab4b29216666a004d6e3ada99958c584735426
Victor Chen <itsVicc@gmail.com>
2012-10-19 15:09:14 -0400

    Changed web layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 HuskyExchange::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/sales',    to: 'static_pages#sales'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
