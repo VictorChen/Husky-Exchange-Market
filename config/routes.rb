bd72a309e391d4b690f60d35ae432622bb69033b
Victor Chen <itsVicc@gmail.com>
2012-11-28 14:45:11 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 HuskyExchange::Application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
@@ -15,6 +21,7 @@ HuskyExchange::Application.routes.draw do
   match '/community', to: 'static_pages#community'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/help', to: 'static_pages#help'  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
