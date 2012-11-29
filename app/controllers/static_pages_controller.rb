294adeb0afa1b777e314e83513547430ce3bde63
Victor Chen <itsVicc@gmail.com>
2012-11-29 18:04:56 -0500

    Fixed pages issue not displaying correctly

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
       @product  = current_user.products.build
       @feed_items = Product.paginate(:page => params[:page], :per_page => 6)
     else
-      @feed_items = Product.find(:all, :limit => 9)
+      @feed_items = Product.paginate(:page => params[:page], :per_page => 9)
     end
   end
 
