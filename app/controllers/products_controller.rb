42ae6899619aa8de2191398959e07c56f8987e0d
Victor Chen <itsVicc@gmail.com>
2012-11-30 16:07:06 -0500

    Added additional categories pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   def create
     @product = current_user.products.build(params[:product])
@@ -12,5 +13,14 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product.destroy
+    redirect_to current_user
   end
+
+  private
+
+    def correct_user
+      @product = current_user.products.find_by_id(params[:id])
+      redirect_to root_url if @product.nil?
+    end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,21 +9,27 @@ class StaticPagesController < ApplicationController
   end
 
   def sales
+    @products  = Product.where(:category => ["electronics", "tickets", "books", "furnitures", "bikes", "tools", "games", "other"]).paginate(:page => params[:page], :per_page => 9)
   end
 
   def housing
+    @products  = Product.where(:category => "housing").paginate(:page => params[:page], :per_page => 9)
   end
 
   def employment
+    @products  = Product.where(:category => "employment").paginate(:page => params[:page], :per_page => 9)
   end
 
   def services
+    @products  = Product.where(:category => "services").paginate(:page => params[:page], :per_page => 9)
   end
 
   def events
+    @products  = Product.where(:category => "events").paginate(:page => params[:page], :per_page => 9)
   end
 
   def community
+    @products  = Product.where(:category => "community").paginate(:page => params[:page], :per_page => 9)
   end
 
   def contact
