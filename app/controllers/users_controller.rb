89636db165adadd00bcd7bc681f6d507ad9c7acb
Victor Chen <itsVicc@gmail.com>
2012-12-04 14:46:03 -0500

    Added ability to edit account and view all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+
   def show
     @user = User.find(params[:id])
     @products = @user.products.paginate(:page => params[:page], :per_page => 12)
@@ -6,6 +8,14 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.paginate(:page => params[:page], :per_page => 10)
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -18,4 +28,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
 end
